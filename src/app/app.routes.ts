ead0d66f3409ec07c2092f9c58bc4d2a1a898fcb
José Martínez <jmartinezt@gmail.com>
2018-02-24 17:16:07 -0600

    Modificaciones en los modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,29 +2,18 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { PagesComponent } from './pages/pages.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
 import { RegisterComponent } from './login/register.component';
+import { PagenotfoundComponent } from './shared/pagenotfound/pagenotfound.component';
 
 
 
 
 
 const appRoutes: Routes = [
-    {path: '', component: PagesComponent,
-    children: [
-        {path: 'dashboard', component: DashboardComponent },
-        {path: 'progress', component: ProgressComponent},
-        {path: 'graficas1', component: Graficas1Component}
-    ]
- },
 
     {path: 'login', component: LoginComponent },
     {path: 'register', component: RegisterComponent},
-    //{path: '', redirectTo: '/dashboard', pathMatch: 'full'},
     {path: '**', component: PagenotfoundComponent}
 ];
 
